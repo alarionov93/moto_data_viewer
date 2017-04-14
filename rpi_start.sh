960bbf1b11eede72da65835051ac498594adb0bf
Alexandr Larionov <alarionov93@yandex.ru>
2017-04-15 00:44:05 +0500

    added start script for rpi

--- rpi_start.sh
+++ rpi_start.sh
@@ -2,9 +2,9 @@
 
 source ./venv/bin/activate
 
-#export LC_ALL=C.UTF-8
-#export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 export FLASK_APP=core_app.py
 flask run --host=0.0.0.0 --port=5002
 
-#cat /dev/tty.HC-05-DevB >> ~/Work/Electronics/dataViewer/mdu.log
+#cat /dev/# some_bt_device # >> ~/moto_data_viewer/mdu.log
