0071636f60b2951536fccae32f4852da0c799e10
Alex Larionov <alarionov93@yandex.ru>
2017-04-14 19:40:15 +0000

    added bt example on rpi, changed migrate script

--- migrate.py
+++ migrate.py
@@ -3,17 +3,17 @@ import models
 models.db.connect()
 
 # This is for 2nd and others migrations
-try:
-    print("Try to remove table Measures...")
-    models.db.drop_table(models.Measures)
-except:
-    print("Table Measures does not exists!")
-finally:
-    print("Try to create table Measures...")
-    models.db.create_table(models.Measures)
+#try:
+#    print("Try to remove table Measures...")
+#    models.db.drop_table(models.Measures)
+#except:
+#    print("Table Measures does not exists!")
+#finally:
+#    print("Try to create table Measures...")
+#    models.db.create_table(models.Measures)
 
 # This id for 1st migration
-#print("Try to create table Measures...")
-#models.db.create_table(models.Measures)
+print("Try to create table Measures...")
+models.db.create_table(models.Measures)
 
 models.db.close()
--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 source ./venv/bin/activate
 
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 export FLASK_APP=core_app.py
 flask run --host=0.0.0.0 --port=5002
 
