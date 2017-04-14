e8ecbff1d23eb4e9219999f488e86eb6fb9ac615
Alex Larionov <alarionov93@yandex.ru>
2017-04-14 20:08:28 +0000

    added examples to bt_ex, changed paths on absolete in rpi start script, added this script to rc.local

--- rpi_start.sh
+++ rpi_start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-source ./venv/bin/activate
+source /home/sanya/moto_data_viewer/venv/bin/activate
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-export FLASK_APP=core_app.py
+export FLASK_APP=/home/sanya/moto_data_viewer/core_app.py
 flask run --host=0.0.0.0 --port=5002
 
 #cat /dev/# some_bt_device # >> ~/moto_data_viewer/mdu.log
